315163c84ea3c4ad9c9e1a00853e8f2dddcfbb2e
smallcjy <2628035541@qq.com>
2025-02-03 13:15:56 +0800

    feat(cli): 添加字符画

--- ncx_client/ncxc.cpp
+++ ncx_client/ncxc.cpp
@@ -2,9 +2,25 @@
 #include "ccontrolchannel.h"
 #include "config.h"
 
+const std::string DRAW_C = R"(
+
+#   __      _____ _    ___ ___  __  __ ___   _____ ___  
+#   \ \    / / __| |  / __/ _ \|  \/  | __| |_   _/ _ \ 
+#    \ \/\/ /| _|| |_| (_| (_) | |\/| | _|    | || (_) |
+#     \_/\_/ |___|____\___\___/|_|  |_|___|   |_| \___/ 
+#                                                       
+#    _  _  _____  _____ _    ___ ___ _  _ _____         
+#   | \| |/ __\ \/ / __| |  |_ _| __| \| |_   _|        
+#   | .` | (__ >  < (__| |__ | || _|| .` | | |          
+#   |_|\_|\___/_/\_\___|____|___|___|_|\_| |_|          
+#                                                       
+
+)";
+
 int
 main(int argc, char* argv[]) 
 {
+    std::cout << DRAW_C << std::endl;
     // read client config 
     ClientConfig config(argv[1]);
     NClient client(config.parseAsControlChannelArgsVec());
--- ncx_server/ncxs.cpp
+++ ncx_server/ncxs.cpp
@@ -3,12 +3,26 @@
 #include "config.h"
 #include <iostream>
 
+const std::string DRAW_S = R"(  
+#   __      _____ _    ___ ___  __  __ ___   _____ ___  
+#   \ \    / / __| |  / __/ _ \|  \/  | __| |_   _/ _ \ 
+#    \ \/\/ /| _|| |_| (_| (_) | |\/| | _|    | || (_) |
+#     \_/\_/ |___|____\___\___/|_|  |_|___|   |_| \___/ 
+#                                                       
+#    _  _  _____  _____ ___ _____   _____ ___           
+#   | \| |/ __\ \/ / __| __| _ \ \ / / __| _ \          
+#   | .` | (__ >  <\__ \ _||   /\ V /| _||   /          
+#   |_|\_|\___/_/\_\___/___|_|_\ \_/ |___|_|_\          
+#                                                       
+)";
+
+
 int 
 main(int argc, char* argv[])
 {
+    std::cout << DRAW_S << std::endl;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
     // read server config 
     ServerConfig config(argv[1]);
-    std::puts("SERVERCONFIG LOAD SUCCESS!");
     NServer ncxs(config.parseAsServerArgs());
     ncxs.run_server();
 }
